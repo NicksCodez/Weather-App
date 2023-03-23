d9ec45b13748e266da4e54b228fea5c5d1430de7
NicksCodez <elisei.nicolae08@gmail.com>
2023-03-23 19:23:54 +0200

    implement example html and css layout

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import './style.css';
 import getApiData from './getApiData';
 import FormatData from './FormatData';
 
-const data = getApiData('Bucharest').then((response) => {
-  // response.forecast.forecastday.forEach((day) => {
-  //   console.log(day);
-  // });
-  const formatData = FormatData(response);
-  const current = formatData.getCurrentWeatherMetric();
-  // console.log(current);
-  const forecast = formatData.getForecastMetric();
-  console.log(forecast);
-});
+// const data = getApiData('Bucharest').then((response) => {
+// response.forecast.forecastday.forEach((day) => {
+//   console.log(day);
+// });
+//   const formatData = FormatData(response);
+//   const current = formatData.getCurrentWeatherMetric();
+//   // console.log(current);
+//   const forecast = formatData.getForecastMetric();
+//   console.log(forecast);
+// });
