f8d6115d4dfd034818594797dff46362df86e406
NicksCodez <elisei.nicolae08@gmail.com>
2023-03-24 18:54:35 +0200

    remove console log

--- src/getApiData.js
+++ src/getApiData.js
@@ -5,7 +5,6 @@ export default async function getData(city) {
   if (typeof city === 'undefined') {
     clientIp = await getIP();
   }
-  console.log({ clientIp });
   const data = await fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=0813d3a5175b44529fd180407232203&q=${
       city || clientIp.ip
